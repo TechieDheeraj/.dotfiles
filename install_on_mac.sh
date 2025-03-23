c95c5060c5d81ab2922d35ef291c36cb705404a0
TechieDheeraj <dheeraj.embdeveloper@gmail.com>
2025-03-23 21:26:29 +0530

    updated bashrc with latest functions

--- install_on_mac.sh
+++ install_on_mac.sh
@@ -8,6 +8,7 @@ brew install cmake
 brew install tmux
 brew install rectangle
 brew install --cask scroll-reverser
+brew install cirruslabs/cli/tart
 sudo port install btop
 
 echo -e "Copying all dotfiles \n"
@@ -23,6 +24,7 @@ cp -rf ./vim/ ~/.vim
 cd ~/.vim/headers/bits; /usr/bin/g++ --std=c++17 -w stdc++.h; cd -; 
 mkdir -p ~/.config/kitty 2>&1 > /dev/null
 cp -rf ./files/config/kitty/ ~/.config/kitty
+cd ~/.config/; git clone https://github.com/jdhao/nvim-config.git nvim 2>&1 > /dev/null ; cd nvim; patch -p1 ./files/nvim.patch
 
 echo -e "All Dotfiles Copied \n"
 vim -c PlugInstall hello -c qa!
